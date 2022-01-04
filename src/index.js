bc166cebdfa5b799f2ea79a03c98f2c64e22d283
Anne-15 <wariiyuanne@gmail.com>
2022-01-04 12:19:12 +0300

    creating a booklist in react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import reactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// css
+import './index.css'
+
+function BookList(){
+  return (
+    <section className='bookList'>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => 
+{
+  return ( 
+  <article className='book'>
+    <Image/>
+    <Title/>
+    <Author/>
+  </article>
+  );
+};
+const Image = () => (
+  <img src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg" 
+  alt="I love you to the moon and back" />
 );
+const Title = () => <h1>I Love you to the Moon and back</h1>;
+const Author = () => <h4>Amelia Hepworth</h4>;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reactDom.render(<BookList/>, document.getElementById('root'));

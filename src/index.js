45097eb4db4419874c2b69611f35afebee75464d
Anne-15 <wariiyuanne@gmail.com>
2022-01-05 10:27:48 +0300

    using arrays to iterate over a list of objects

--- src/index.js
+++ src/index.js
@@ -4,36 +4,50 @@ import reactDom from 'react-dom';
 // css
 import './index.css'
 
+// setup variables
+
+const books = [
+  {
+    id: 1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81kYj5Uyu8L._AC_UL302_SR302,200_.jpg',
+    title: 'The Love Hypothesis',
+    author: 'Ali Hazelwood'
+  },
+  {
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81kYj5Uyu8L._AC_UL302_SR302,200_.jpg',
+    title: 'The Love Hypothesis',
+    author: 'Ali Hazelwood'
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71EwoNQypZL._AC_UL302_SR302,200_.jpg',
+    title: 'It Ends with Us: A Novel',
+    author: 'Colleen Hoover'
+  }
+];
+
 function BookList(){
   return (
     <section className='bookList'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {books.map((book) => {
+        const {img, title, author } = book;
+        return <Book key = {book.id} book = {book}></Book>;
+      })}
     </section>
   );
 }
 
-const Book = () => 
-{
+const Book = (props) => {
+  const { img, title, author } = props.book
   return ( 
   <article className='book'>
-    <Image/>
-    <Title/>
-    <Author/>
+    <img src = {img} alt="I love you to the moon and back"/>
+    <h1>{title}</h1>
+    <h4>{author}</h4>
   </article>
   );
 };
-const Image = () => (
-  <img src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL604_SR604,400_.jpg" 
-  alt="I love you to the moon and back" />
-);
-const Title = () => <h1>I Love you to the Moon and back</h1>;
-const Author = () => <h4>Amelia Hepworth</h4>;
+
 
 reactDom.render(<BookList/>, document.getElementById('root'));
